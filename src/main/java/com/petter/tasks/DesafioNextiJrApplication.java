af8e1929739bdf3be16d8524656d87a5602bf0ac
petterson <pettersonsueder@hotmail.com.br>
2021-07-21 13:22:35 -0300

    segundo comite

---
+++ src/main/java/com/petter/tasks/DesafioNextiJrApplication.java
@@ -0,0 +1,13 @@
+package com.petter.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNextiJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNextiJrApplication.class, args);
+	}
+
+}
